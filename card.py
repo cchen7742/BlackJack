961fe50cb37ee409b2254f4f602151ea2a637f1c
chiahche <chiahche@umich.edu>
2018-05-01 21:35:55 -0400

    Initial commit

---
+++ card.py
@@ -0,0 +1,15 @@
+class Card: 
+
+	suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+	ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 
+			'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+	values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 
+			'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,'Queen':10, 'King':10, 'Ace':11}
+
+	def __init__(self):
+		self.suits
+		self.ranks
+		self.values
+
+	def __str__(self):
+		pass
